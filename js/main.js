6c243cabdb5fc1eeb07b5e76aa195b48b0f2af6b
evox1994 <dmitrybatukhtin@gmail.com>
2018-09-19 12:37:35 +0700

    ыавпвапвп

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	function scrollHeader() {
+	function scrollHeader(){
 		var st = $(window).scrollTop();
 		
 		if ( $(window).width() > 768 ) {
@@ -15,7 +15,7 @@ $(document).ready(function(){
 	}
 	scrollHeader();
 
-	function liNumber() {
+	function liNumber(){
 		var i = 0;
 		$('.b-3-list .li-name').find('span b').each(function(){
 			i++;
@@ -23,6 +23,33 @@ $(document).ready(function(){
 		});
 	}
 	liNumber();
+
+	function filter(){
+		if ( $('.filter li.active').length ) {
+			var f = $('.filter li.active').data('filter');
+
+			$('.b-9-list li').removeClass('active');
+			setTimeout(function(){
+				$('.b-9-list li').css('display','none');
+				$(f).css('display','inline-block');
+				setTimeout(function(){$(f).addClass('active');},100);
+			},300);
+		} else {
+			$('.b-9-list li').css('display','inline-block');
+			setTimeout(function(){$('.b-9-list li').addClass('active');},100);
+		}
+	}
+	filter();
+
+	$('.filter li').click(function(){
+		if ( $(this).hasClass('active') ) {
+			$(this).removeClass('active');
+		} else {
+			$('.filter li').removeClass('active');
+			$(this).addClass('active');
+		}
+		filter();
+	});
 
 	$('.reviews-slider').slick();
 
